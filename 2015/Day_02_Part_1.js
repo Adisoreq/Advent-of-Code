e0196ceca27d9c66919ecd9bb2a11097359c0665
Adisor <adisorpl@gmail.com>
2025-01-16 12:10:27 +0100

    Day 2 Part 1

--- 2015/Day_02_Part_1.js
+++ 2015/Day_02_Part_1.js
@@ -2,7 +2,6 @@
 const input = `
 2x3x4
 1x1x10
-
 ` 
 
 const areas = []
@@ -11,10 +10,10 @@ let total = 0
 const dimensions = input.match(/.+/gm).map(row => row.split('x').map(Number))
 
 for (let d of dimensions) {
-    const w = d[0]
-    const h = d[1]
-    const l = d[2]
-    areas.push(2*w*h + 2*w*l + 2*l*h)
+    const wh = d[0] * d[1]
+    const hl = d[1] * d[2]
+    const lw = d[2] * d[0]
+    areas.push(2*wh + 2*hl + 2*lw + Math.min(wh,hl,lw))
 }
 
 for (let i = 0; i < areas.length; ++i) {
