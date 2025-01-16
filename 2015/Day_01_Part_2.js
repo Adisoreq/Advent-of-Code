9e42cdceb430eb3722b2893d71aa7ecfa398f997
Adisor <adisorpl@gmail.com>
2025-01-16 11:48:23 +0100

    Day 1 Part 2

---
+++ 2015/Day_01_Part_2.js
@@ -0,0 +1,21 @@
+const input = `()())` // Insert actual input here
+
+var floor = 0;
+var steps = 0;
+
+for (let i = 0; i < input.length; ++i) {
+    
+    if (floor === -1) {
+        break;
+    }
+    
+    if (input[i] === '(')
+        ++floor;
+    else if (input[i] === ')')
+        --floor;
+        
+    ++steps;
+    console.log(`${steps}. ${floor}`)
+}
+
+console.log(`Steps: ${steps}`)
