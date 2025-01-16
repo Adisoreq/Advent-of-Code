7ba2c5c1c642ad07176d7bd3af056bac6654e2c4
Adisor <adisorpl@gmail.com>
2025-01-16 20:32:54 +0100

    Day 03 Part 1

---
+++ 2015/Day_03_Part_1.js
@@ -0,0 +1,30 @@
+const input = `^>v<` // Insert actual input here
+
+const Directions = new Map([
+    [ '^', [0,1] ], // North
+    [ '>', [1,0] ], // East
+    [ 'v', [0,-1] ], // South
+    [ '<', [-1,0] ], // West
+])
+
+var position = [0, 0]
+
+function move(direction) {
+    const deltaPos = Directions.get(direction)
+    position[0] += deltaPos[0]
+    position[1] += deltaPos[1]
+}
+
+const visited = new Set([ `${position[0]},${position[1]}` ])
+let count = 1
+
+for (let char of input) {
+    move(char)
+    const posString = `${position[0]},${position[1]}`
+    if (!visited.has(posString)) {
+        visited.add(posString)
+        count += 1
+    }
+}
+
+console.log(count)
